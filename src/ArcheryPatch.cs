e8cfe88cd9fb1aa4224dcaeabd4c38546d6e4eed
Ezinw <112014367+Ezinw@users.noreply.github.com>
2023-11-13 17:13:10 -0400

    Add files via upload

---
+++ src/ArcheryPatch.cs
@@ -0,0 +1,273 @@
+using HarmonyLib;
+using Il2Cpp;
+
+namespace SkillAdjustmentArchery
+{
+    [HarmonyPatch(typeof(SkillsManager), "Awake")]
+    internal class SkillAdjustment
+    {
+
+        public static void Postfix(SkillsManager __instance)
+        {
+            __instance.m_Skill_Archery.m_LevelWhereCanFireFromCrouch = Settings.settings.CrouchLevel;
+
+            __instance.m_Skill_Archery.m_SwayReduction[0] = Settings.settings.Sway0;
+            __instance.m_Skill_Archery.m_DamageIncrease[0] = Settings.settings.Damage0;
+            __instance.m_Skill_Archery.m_CriticalHitChanceIncrease[0] = Settings.settings.Critical0;
+            __instance.m_Skill_Archery.m_BleedOutTimeReduction[0] = Settings.settings.BleedOut0;
+            __instance.m_Skill_Archery.m_ConditionDegradeOnUseReduction[0] = Settings.settings.PerUseConditionLoss0;
+
+            __instance.m_Skill_Archery.m_SwayReduction[1] = Settings.settings.Sway1;
+            __instance.m_Skill_Archery.m_DamageIncrease[1] = Settings.settings.Damage1;
+            __instance.m_Skill_Archery.m_CriticalHitChanceIncrease[1] = Settings.settings.Critical1;
+            __instance.m_Skill_Archery.m_BleedOutTimeReduction[1] = Settings.settings.BleedOut1;
+            __instance.m_Skill_Archery.m_ConditionDegradeOnUseReduction[1] = Settings.settings.PerUseConditionLoss1;
+
+            __instance.m_Skill_Archery.m_SwayReduction[2] = Settings.settings.Sway2;
+            __instance.m_Skill_Archery.m_DamageIncrease[2] = Settings.settings.Damage2;
+            __instance.m_Skill_Archery.m_CriticalHitChanceIncrease[2] = Settings.settings.Critical2;
+            __instance.m_Skill_Archery.m_BleedOutTimeReduction[2] = Settings.settings.BleedOut2;
+            __instance.m_Skill_Archery.m_ConditionDegradeOnUseReduction[2] = Settings.settings.PerUseConditionLoss2;
+
+            __instance.m_Skill_Archery.m_SwayReduction[3] = Settings.settings.Sway3;
+            __instance.m_Skill_Archery.m_DamageIncrease[3] = Settings.settings.Damage3;
+            __instance.m_Skill_Archery.m_CriticalHitChanceIncrease[3] = Settings.settings.Critical3;
+            __instance.m_Skill_Archery.m_BleedOutTimeReduction[3] = Settings.settings.BleedOut3;
+            __instance.m_Skill_Archery.m_ConditionDegradeOnUseReduction[3] = Settings.settings.PerUseConditionLoss3;
+
+            __instance.m_Skill_Archery.m_SwayReduction[4] = Settings.settings.Sway4;
+            __instance.m_Skill_Archery.m_DamageIncrease[4] = Settings.settings.Damage4;
+            __instance.m_Skill_Archery.m_CriticalHitChanceIncrease[4] = Settings.settings.Critical4;
+            __instance.m_Skill_Archery.m_BleedOutTimeReduction[4] = Settings.settings.BleedOut4;
+            __instance.m_Skill_Archery.m_ConditionDegradeOnUseReduction[4] = Settings.settings.PerUseConditionLoss4;
+
+        }
+
+    }
+
+
+    [HarmonyPatch(typeof(Skill_Archery), "GetTierBenefits")]
+        public class ArcheryBenefits
+        {
+            static void Postfix(ref string __result, Skill_Archery __instance)
+            {
+                SkillTiers currentTier = (SkillTiers)__instance.GetCurrentTierNumber();
+
+            //crouch
+            if (currentTier == SkillTiers.Beginner && Settings.settings.CrouchLevel == 1 ||
+                currentTier == SkillTiers.Novice && Settings.settings.CrouchLevel == 2   ||
+                currentTier == SkillTiers.Novice && Settings.settings.CrouchLevel == 1   ||
+                currentTier == SkillTiers.Skilled && Settings.settings.CrouchLevel == 3  ||
+                currentTier == SkillTiers.Skilled && Settings.settings.CrouchLevel == 2  ||
+                currentTier == SkillTiers.Skilled && Settings.settings.CrouchLevel == 1  ||
+                currentTier == SkillTiers.Expert && Settings.settings.CrouchLevel == 4   ||
+                currentTier == SkillTiers.Expert && Settings.settings.CrouchLevel == 3   ||
+                currentTier == SkillTiers.Expert && Settings.settings.CrouchLevel == 2   ||
+                currentTier == SkillTiers.Expert && Settings.settings.CrouchLevel == 1)
+                          { __result += "\nCan fire bow when crouched";}
+
+            //lvl 1
+            if (currentTier == SkillTiers.Beginner && Settings.settings.Sway0 >= 1) {__result += $"\nBow sway reduced by {Settings.settings.Sway0}%";}
+            if (currentTier == SkillTiers.Beginner && Settings.settings.Damage0 >= 1) {__result += $"\nArrow damage increased by {Settings.settings.Damage0}%";}
+            if (currentTier == SkillTiers.Beginner && Settings.settings.Critical0 >= 1) {__result += $"\nCritical hit chance increased by {Settings.settings.Critical0}%";}
+            if (currentTier == SkillTiers.Beginner && Settings.settings.BleedOut0 >= 1) {__result += $"\nBleed out time reduced by {Settings.settings.BleedOut0}%";}
+            if (currentTier == SkillTiers.Beginner && Settings.settings.PerUseConditionLoss0 >= 1) {__result += $"\nPer-use bow condition loss reduced by {Settings.settings.PerUseConditionLoss0}%";}
+
+            //lvl 2
+            if (currentTier == SkillTiers.Novice && Settings.settings.Critical1 >= 1) { __result += $"\nCritical hit chance increased by {Settings.settings.Critical1}%"; }
+            if (currentTier == SkillTiers.Novice && Settings.settings.BleedOut1 >= 1) { __result += $"\nBleed out time reduced by {Settings.settings.BleedOut1}%"; }
+            if (currentTier == SkillTiers.Novice && Settings.settings.PerUseConditionLoss1 >= 1) { __result += $"\nPer-use bow condition loss reduced by {Settings.settings.PerUseConditionLoss1}%"; }
+            if (currentTier == SkillTiers.Novice && Settings.settings.Sway1 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Sway: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Bow sway reduced by: {Settings.settings.Sway1}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Novice && Settings.settings.Damage1 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Arrow: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Arrow damage increased by: {Settings.settings.Damage1}%";
+                }
+
+            }
+
+            //lvl 3
+            if (currentTier == SkillTiers.Skilled && Settings.settings.BleedOut2 >= 1) { __result += $"\nBleed out time reduced by {Settings.settings.BleedOut2}%"; }
+            if (currentTier == SkillTiers.Skilled && Settings.settings.PerUseConditionLoss2 >= 1) { __result += $"\nPer-use bow condition loss reduced by {Settings.settings.PerUseConditionLoss2}%"; }
+            if (currentTier == SkillTiers.Novice && Settings.settings.Sway2 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Sway: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Bow sway reduced by: {Settings.settings.Sway2}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Skilled && Settings.settings.Damage2 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Arrow: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Arrow damage increased by: {Settings.settings.Damage2}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Skilled && Settings.settings.Critical2 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Critical: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Critical hit chance increased by: {Settings.settings.Critical2}%";
+                }
+
+            }
+
+            //lvl 4
+            if (currentTier == SkillTiers.Expert && Settings.settings.Sway3 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Sway: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Bow sway reduced by: {Settings.settings.Sway3}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Expert && Settings.settings.Damage3 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Arrow: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Arrow damage increased by: {Settings.settings.Damage3}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Expert && Settings.settings.Critical3 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Critical: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Critical hit chance increased by: {Settings.settings.Critical3}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Expert && Settings.settings.BleedOut3 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Bleed: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Bleed out time reduced by: {Settings.settings.BleedOut3}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Expert && Settings.settings.PerUseConditionLoss3 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Condition: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Per-use bow condition loss reduced by: {Settings.settings.PerUseConditionLoss3}%";
+                }
+
+            }
+
+            //lvl 5
+            if (currentTier == SkillTiers.Master && Settings.settings.Sway4 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Sway: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Bow sway reduced by: {Settings.settings.Sway4}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Master && Settings.settings.Damage4 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Arrow: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Arrow damage increased by: {Settings.settings.Damage4}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Master && Settings.settings.Critical4 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Crit: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Critical hit chance increased by: {Settings.settings.Critical4}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Master && Settings.settings.BleedOut4 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Bleed: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Bleed out time reduced by: {Settings.settings.BleedOut4}%";
+                }
+
+            }
+            if (currentTier == SkillTiers.Master && Settings.settings.PerUseConditionLoss4 >= 1)
+            {
+                int existingBenefitIndex = __result.IndexOf("Condition: ");
+                if (existingBenefitIndex != -1)
+                {
+                    int endOfLineIndex = __result.IndexOf('\n', existingBenefitIndex);
+                    if (endOfLineIndex != -1)
+                    { __result = __result.Remove(existingBenefitIndex, endOfLineIndex - existingBenefitIndex); }
+                    __result += $"Per-use bow condition loss reduced by: {Settings.settings.PerUseConditionLoss4}%";
+                }
+
+            }
+
+        }
+
+    }
+    
+}
---
+++ src/Settings.cs
@@ -0,0 +1,234 @@
+﻿using ModSettings;
+using System.Reflection;
+
+namespace SkillAdjustmentArchery
+{
+    internal class Settings : JsonModSettings
+    {
+
+        [Section("Archery")]
+
+        [Name("Archery Skills")]
+        private readonly bool Archery = true;
+
+        [Name("Fire Bow While Crouched")]
+        [Description("Skill level required to fire bow while crouched. (Game default = 5) - Requires game reload to take effect")]
+        [Slider(1, 5)]
+        public int CrouchLevel = 5;
+
+        [Name("Archery Level 1 ------------------------------")]
+        [Description("Show or hide level 1 skills - All changes made in game require a game reload to take effect")]
+        [Choice("+", "-")]
+        public bool Archery1 = false;
+
+        [Name("          Bow Sway Reduction")]
+        [Description("Reduce bow sway. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int Sway0 = 0;
+
+        [Name("          Damage Increase")]
+        [Description("Increase Damage. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int Damage0 = 0;
+
+        [Name("          Critical Hit Chance")]
+        [Description("Increase critical hit chance. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int Critical0 = 0;
+
+        [Name("          Bleed Out Time Reduction")]
+        [Description("Increase the time for animals to bleed out. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int BleedOut0 = 0;
+
+        [Name("          Condition loss")]
+        [Description("Reduce the amount of condition loss each use. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int PerUseConditionLoss0 = 0;
+
+        [Name("Archery Level 2 ------------------------------")]
+        [Description("Show or hide level 2 skills - All changes made in game require a game reload to take effect")]
+        [Choice("+", "-")]
+        public bool Archery2 = false;
+
+        [Name("          Bow Sway Reduction")]
+        [Description("Reduce bow sway. (Game default = 25%)")]
+        [Slider(0, 100)]
+        public int Sway1 = 25;
+
+        [Name("          Damage Increase")]
+        [Description("Increase Damage. (Game default = 10%)")]
+        [Slider(0, 100)]
+        public int Damage1 = 10;
+
+        [Name("          Critical Hit Chance")]
+        [Description("Increase critical hit chance. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int Critical1 = 0;
+
+        [Name("          Bleed Out Time Reduction")]
+        [Description("Increase the time for animals to bleed out. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int BleedOut1 = 0;
+
+        [Name("          Condition loss")]
+        [Description("Reduce the amount of condition loss each use. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int PerUseConditionLoss1 = 0;
+
+        [Name("Archery Level 3 ------------------------------")]
+        [Description("Show or hide level 3 skills - All changes made in game require a game reload to take effect")]
+        [Choice("+", "-")]
+        public bool Archery3 = false;
+
+        [Name("          Bow Sway Reduction")]
+        [Description("Reduce bow sway. (Game default = 50%)")]
+        [Slider(0, 100)]
+        public int Sway2 = 50;
+
+        [Name("          Damage Increase")]
+        [Description("Increase Damage. (Game default = 10%)")]
+        [Slider(0, 100)]
+        public int Damage2 = 10;
+
+        [Name("          Critical Hit Chance")]
+        [Description("Increase critical hit chance. (Game default = 15%)")]
+        [Slider(0, 100)]
+        public int Critical2 = 15;
+
+        [Name("          Bleed Out Time Reduction")]
+        [Description("Increase the time for animals to bleed out. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int BleedOut2 = 0;
+
+        [Name("          Condition loss")]
+        [Description("Reduce the amount of condition loss each use. (Game default = 0%)")]
+        [Slider(0, 100)]
+        public int PerUseConditionLoss2 = 0;
+
+        [Name("Archery Level 4 ------------------------------")]
+        [Description("Show or hide level 4 skills - All changes made in game require a game reload to take effect")]
+        [Choice("+", "-")]
+        public bool Archery4 = false;
+
+        [Name("          Bow Sway Reduction")]
+        [Description("Reduce bow sway. (Game default = 75%)")]
+        [Slider(0, 100)]
+        public int Sway3 = 75;
+
+        [Name("          Damage Increase")]
+        [Description("Increase Damage. (Game Default = 10%)")]
+        [Slider(0, 100)]
+        public int Damage3 = 10;
+
+        [Name("          Critical Hit Chance")]
+        [Description("Increase critical hit chance. (Game default = 25%)")]
+        [Slider(0, 100)]
+        public int Critical3 = 25;
+
+        [Name("          Bleed Out Time Reduction")]
+        [Description("Increase the time for animals to bleed out. (Game default = 25%)")]
+        [Slider(0, 100)]
+        public int BleedOut3 = 25;
+
+        [Name("          Condition loss")]
+        [Description("Reduce the amount of condition loss each use. (Game default = 50%)")]
+        [Slider(0, 100)]
+        public int PerUseConditionLoss3 = 50;
+
+        [Name("Archery Level 5 ------------------------------")]
+        [Description("Show or hide level  skills - All changes made in game require a game reload to take effect")]
+        [Choice("+", "-")]
+        public bool Archery5 = false;
+
+        [Name("          Bow Sway Reduction")]
+        [Description("Reduce bow sway. (Game default = 75%)")]
+        [Slider(0, 100)]
+        public int Sway4 = 75;
+
+        [Name("          Damage Increase")]
+        [Description("Increase Damage. (Game default = 25%)")]
+        [Slider(0, 100)]
+        public int Damage4 = 25;
+
+        [Name("          Critical Hit Chance")]
+        [Description("=Increase critical hit chance. (Game default = 50%)")]
+        [Slider(0, 100)]
+        public int Critical4 = 50;
+
+        [Name("          Bleed Out Time Reduction")]
+        [Description("Increase the time for animals to bleed out. (Game default = 50%)")]
+        [Slider(0, 100)]
+        public int BleedOut4 = 50;
+
+        [Name("          Condition loss")]
+        [Description("Reduce the amount of condition loss each use. (Game default = 50%)")]
+        [Slider(0, 100)]
+        public int PerUseConditionLoss4 = 50;
+
+
+        protected override void OnChange(FieldInfo field, object oldValue, object newValue)
+        {
+            if (field.Name == nameof(Archery)   ||
+                field.Name == nameof(Archery1)  ||
+                field.Name == nameof(Archery2)  ||
+                field.Name == nameof(Archery3)  ||
+                field.Name == nameof(Archery4)  ||
+                field.Name == nameof(Archery5))
+            {
+                RefreshFields();
+            }
+        }
+
+        internal void RefreshFields()
+        {
+            //ARCHERY
+            SetFieldVisible(nameof(CrouchLevel), Archery);
+
+            SetFieldVisible(nameof(Archery1), Archery);
+            SetFieldVisible(nameof(Sway0), (Archery1) && Archery);
+            SetFieldVisible(nameof(Damage0), (Archery1) && Archery);
+            SetFieldVisible(nameof(Critical0), (Archery1) && Archery);
+            SetFieldVisible(nameof(BleedOut0), (Archery1) && Archery);
+            SetFieldVisible(nameof(PerUseConditionLoss0), (Archery1) && Archery);
+
+            SetFieldVisible(nameof(Archery2), Archery);
+            SetFieldVisible(nameof(Sway1), (Archery2) && Archery);
+            SetFieldVisible(nameof(Damage1), (Archery2) && Archery);
+            SetFieldVisible(nameof(Critical1), (Archery2) && Archery);
+            SetFieldVisible(nameof(BleedOut1), (Archery2) && Archery);
+            SetFieldVisible(nameof(PerUseConditionLoss1), (Archery2) && Archery);
+
+            SetFieldVisible(nameof(Archery3), Archery);
+            SetFieldVisible(nameof(Sway2), (Archery3) && Archery);
+            SetFieldVisible(nameof(Damage2), (Archery3) && Archery);
+            SetFieldVisible(nameof(Critical2), (Archery3) && Archery);
+            SetFieldVisible(nameof(BleedOut2), (Archery3) && Archery);
+            SetFieldVisible(nameof(PerUseConditionLoss2), (Archery3) && Archery);
+
+            SetFieldVisible(nameof(Archery4), Archery);
+            SetFieldVisible(nameof(Sway3), (Archery4) && Archery);
+            SetFieldVisible(nameof(Damage3), (Archery4) && Archery);
+            SetFieldVisible(nameof(Critical3), (Archery4) && Archery);
+            SetFieldVisible(nameof(BleedOut3), (Archery4) && Archery);
+            SetFieldVisible(nameof(PerUseConditionLoss3), (Archery4) && Archery);
+
+            SetFieldVisible(nameof(Archery5), Archery);
+            SetFieldVisible(nameof(Sway4), (Archery5) && Archery);
+            SetFieldVisible(nameof(Damage4), (Archery5) && Archery);
+            SetFieldVisible(nameof(Critical4), (Archery5) && Archery);
+            SetFieldVisible(nameof(BleedOut4), (Archery5) && Archery);
+            SetFieldVisible(nameof(PerUseConditionLoss4), (Archery5) && Archery);
+
+        }
+
+
+        internal static Settings settings;
+        internal static void OnLoad()
+        {
+            settings = new Settings();
+            settings.AddToModSettings("Skill-Adjustment-Archery");
+            settings.RefreshFields();
+        }
+    }
+}
